3417914ed843e73b726d18a98db30d8492277b13
a.sabzalian <asabzalain@medrick.info>
2024-03-10 17:55:29 +0330

    add settings

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,18 +1,19 @@
 import 'dotenv/config';
 import mongoose from "mongoose";
 import app from "./app";
+import { settings } from './settings';
 
 async function start() {
-    if (!process.env.JWT_SECRET) {
+    if (!settings.JWT.secretKey) {
         throw new Error("JWT_SECRET must be defined");
     }
 
-    if (!process.env.MONGO_URI) {
+    if (!settings.database.MONGO_URI) {
         throw new Error("MONGO_URI must be defined");
     }
 
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(settings.database.MONGO_URI);
         console.log("Connected to MongoDB.......!");
     } catch (err) {
         console.error(err);
--- auth/src/settings.ts
+++ auth/src/settings.ts
@@ -3,5 +3,9 @@ export const settings = {
         accessTokenLife: process.env.JWT_ACCESS_LIFE ? process.env.JWT_ACCESS_LIFE : '1d',
         refreshTokenLife: process.env.JWT_ACCESS_LIFE ? process.env.JWT_ACCESS_LIFE : '7d',
         secretKey: process.env.JWT_SECRET ? process.env.JWT_SECRET : '125412)(*)(835-wepok(*)(&$@)'
+    },
+
+    database: {
+        MONGO_URI: process.env.MONGO_URI ? process.env.MONGO_URI : 'mongodb://localhost:27017/authDatabase'
     }
 }
